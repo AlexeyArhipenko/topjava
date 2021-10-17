add43dc4e75904449f781d1b2c5a79863a51cd3c
Alexej Arhipenko <alexej.arhipenko1990@gmail.com>
2021-10-17 14:48:33 +0700

    add_logging

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,13 +1,22 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class UserServlet extends HttpServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(UserServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        LOG.debug("redirect to users");
+        //        request.getRequestDispatcher("/users.jsp").forward(request, response);
+                response.sendRedirect("users.jsp");
     }
 
     @Override
