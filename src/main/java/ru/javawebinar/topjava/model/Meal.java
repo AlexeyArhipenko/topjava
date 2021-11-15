6ff13585b69a2954f3d38fa3e6077024f230651c
Alexej Arhipenko <alexej.arhipenko1990@gmail.com>
2021-11-15 21:38:15 +0700

    Refactoring

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,9 +5,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-public class Meal {
+public class Meal extends AbstractBaseEntity {
 
-    private Integer id;
     private final LocalDateTime dateTime;
     private final String description;
     private final int calories;
@@ -17,15 +16,12 @@ public class Meal {
     }
 
     public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
-        this.id = id;
+        super(id);
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     public LocalDateTime getDateTime() {
         return dateTime;
@@ -45,10 +41,6 @@ public class Meal {
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
-    }
-
-    public Integer getId() {
-        return id;
     }
 
     public boolean isNew() {
