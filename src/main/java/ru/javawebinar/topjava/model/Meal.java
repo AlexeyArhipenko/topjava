c90c9977c724bb718b336b9f462e636625c2a071
Alexej Arhipenko <alexej.arhipenko1990@gmail.com>
2021-12-26 22:43:19 +0700

    Implemented JdbcMealRepository and fix MealServlet

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -6,9 +6,12 @@ import java.time.LocalTime;
 
 public class Meal extends AbstractBaseEntity {
 
-    private final LocalDateTime dateTime;
-    private final String description;
-    private final int calories;
+    private LocalDateTime dateTime;
+    private String description;
+    private int calories;
+
+    public Meal() {
+    }
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this(null, dateTime, description, calories);
@@ -21,6 +24,17 @@ public class Meal extends AbstractBaseEntity {
         this.calories = calories;
     }
 
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
 
     public LocalDateTime getDateTime() {
         return dateTime;
