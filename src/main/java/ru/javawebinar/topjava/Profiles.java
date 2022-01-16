be09879b7d0dceaf2cdff591a09821831ab1a03b
Alexej Arhipenko <alexej.arhipenko1990@gmail.com>
2022-01-16 17:46:32 +0700

    Add datajpa

---
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava;
+
+public class Profiles {
+    public static final String
+            JDBC = "jdbc",
+            JPA = "jpa",
+            DATAJPA = "datajpa";
+
+    public static final String REPOSITORY_IMPLEMENTATION = DATAJPA;
+}
